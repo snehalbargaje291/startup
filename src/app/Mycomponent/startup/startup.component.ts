e975f9eb6720f7ab9d4328b75efd66fdcb1055b5
snehalbargaje291 <snehal.bargaje291@gmail.com>
2024-02-11 02:01:29 +0530

    solved removed

--- src/app/Mycomponent/startup/startup.component.ts
+++ src/app/Mycomponent/startup/startup.component.ts
@@ -11,37 +11,38 @@ import { RouterModule, RouterLink, RouterLinkActive, RouterOutlet, Router } from
 export class StartupComponent {
   constructor(private router: Router) {}
 
-  displayTextSignup = 'Signup';
+  
   displayTextAbout = 'Services';
-  initialRouterLinkSignup = './signup';
   initialRouterLinkAbout = './services';
-
-  changeToLogin() {
-    if (this.displayTextSignup === 'Signup') {
-      this.displayTextSignup = 'Signin';
-      this.initialRouterLinkSignup = './login';
-    }
-  }
-
-  changeToSignup() {
-    if (this.displayTextSignup === 'Signin') {
-      this.displayTextSignup = 'Signup';
-      this.initialRouterLinkSignup = './signup';
-    }
-  }
 
   changeToContact() {
     this.displayTextAbout = 'Contact';
     this.initialRouterLinkAbout = './contact';
   }
 
-  handleSignupClick() {
-    if (this.displayTextSignup === 'Signin') {
-      this.changeToSignup();
-    } else {
-      this.changeToLogin();
-    }
-  }
+   displayTextSignup = 'Signup';
+   initialRouterLinkSignup = './signup';
+  // changeToLogin() {
+  //   if (this.displayTextSignup === 'Signup') {
+  //     this.displayTextSignup = 'Signin';
+  //     this.initialRouterLinkSignup = './login';
+  //   }
+  // }
+
+  // changeToSignup() {
+  //   if (this.displayTextSignup === 'Signin') {
+  //     this.displayTextSignup = 'Signup';
+  //     this.initialRouterLinkSignup = './signup';
+  //   }
+  // }
+
+  // handleSignupClick() {
+  //   if (this.displayTextSignup = 'Signup') {
+  //     this.changeToSignup();
+  //   } else {
+  //     this.changeToLogin();
+  //   }
+  // }
   
   resetTextAndRouterLink() {
     this.displayTextSignup = 'Signup';
