ac3549cd60e7660d80fd2c5e9f0d407209c063e1
snehalbargaje291 <snehal.bargaje291@gmail.com>
2024-02-14 17:22:45 +0530

    changes

--- src/app/Mycomponent/cart/cart.component.ts
+++ src/app/Mycomponent/cart/cart.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule,RouterLink,RouterLinkActive,RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CartComponent, RouterModule,RouterLink,RouterLinkActive,RouterOutlet],
+  imports: [CartComponent, FormsModule, RouterModule,RouterLink,RouterLinkActive,RouterOutlet],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
 export class CartComponent {
-
+  price= 650;
+  quantity: any = 1;
+  total: any;
+  price1= 550;
+  quantity1: any = 1;
+  total1: any;
+  subTotal: any;
+  ngOnInit(){
+    
+  }
+  upDateCart(){
+    this.total = this.price * this.quantity;
+    this.total1 = this.price1 * this.quantity1;
+    this.subTotal = this.total + this.total1;
+  }
 }
